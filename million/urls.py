81a83dbefcc74f97eaa508c9ebfd99608f80f322
codefisher <mail@codefisher.org>
2015-06-07 20:11:06 +0200

    The game mostly works now.

--- million/urls.py
+++ million/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
-from .views import index, game, question
+from .views import index, game, question, play
 
 urlpatterns = [
-    url(r'^$', index),
+    url(r'^$', index, name='million'),
     url(r'^game/(?P<game_id>\d*)/$', game, name="million-game"),
+    url(r'^game/(?P<game_id>\d*)/play/$', play, name="million-play"),
     url(r'^question/(?P<question_id>\d*)/$', question, name="million-question"),
 ]
--- million/views.py
+++ million/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.urlresolvers import reverse
 from .models import Game, Question
 # Create your views here.
 
@@ -11,13 +12,50 @@ def index(request):
 def game(request, game_id):
     game = get_object_or_404(Game, pk=game_id)
     questions = Question.objects.filter(game=game).order_by("value")
+    request.session['game'] = game.pk
+    request.session['question'] = 0
     return render(request, "million/game.html", {
         "game": game,
         "questions": questions,
     })
 
+def play(request, game_id):
+    game = get_object_or_404(Game, pk=game_id)
+    questions = Question.objects.filter(game=game).order_by("value")
+    if request.session.get('game') != game.pk:
+        request.session['game'] = game.pk
+        request.session['question'] = 0
+        return redirect(reverse("million-question", kwargs={"question_id": questions[request.session.get('question')].pk}))
+    if request.method == 'POST':
+        question = get_object_or_404(Question, pk=request.POST.get('question'))
+        answer = request.POST.get('answer')
+        correct = ((answer == 'one' and question.answer_one_correct)
+            or (answer == 'two' and question.answer_two_correct)
+            or (answer == 'three' and question.answer_three_correct)
+            or (answer == 'four' and question.answer_four_correct))
+        if correct:
+            request.session['question'] += 1
+        else:
+            request.session['question'] = 0
+        if request.session['question'] >= len(questions):
+            return render(request, "million/million.html", {})
+        return render(request, "million/result.html", {
+            "game": game,
+            "questions": questions,
+            "correct": correct,
+            "current": request.session['question'],
+        })
+    try:
+        return redirect(reverse("million-question", kwargs={"question_id": questions[request.session.get('question')].pk}))
+    except IndexError:
+        return redirect(reverse("million-game", kwargs={"game_id": game.pk}))
+
 def question(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    questions = Question.objects.filter(game=question.game).order_by("value")
     return render(request, "million/question.html", {
+        "game": question.game,
         "question": question,
+        "questions": questions,
+        "current": request.session['question'],
     })
